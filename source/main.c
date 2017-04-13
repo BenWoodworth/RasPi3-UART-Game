4e155d8d61d0771d93c38911899641a8f4abd493
Ben Woodworth <ben@benwoodworth.net>
2017-04-13 00:47:56 -0400

    Implement main(), setting up dependencies.

--- source/main.c
+++ source/main.c
@@ -1,35 +1,24 @@
-#include "gpio.h"
-#include "system_timer.h"
-#include "uart.h"
-
-void blink_once()
-{
-    // Turn on GPIO 24
-    gpio[GPSET0] |= 1 << 24;
-
-    timer_delay_ms(500);
-
-    // Turn off GPIO 24
-    gpio[GPCLR0] |= 1 << 24;
-
-    timer_delay_ms(500);
-}
-
-void blink_code(uint32_t err)
-{
-    for(int i = 0; i < err; ++i)
-    {
-        blink_once();
-    }
-
-    // Only delay 4 seconds, since we delay for 1 additional
-    // second in blink_once().
-    timer_delay_ms(4500);
-}
 
 int main()
 {
-    // Implement Lab 4 as described in the lab manual
+    // Initialize dependencies
+    Terminal terminal;
+    Timer timer;
+    #if defined COMPILE_GCC
+        terminal = new TerminalConsole();
+        timer = new TimerGcc();
+    #elif defined COMPILE_RASPI
+        terminal = new TerminalUart();
+        timer = new TimerRasPi();
+    #else
+        #error "Compile with '-D COMPILE_GCC' or '-D COMPILE_RASPI'"
+    #endif
     
+    // Initialize and start game manager
+    /*
+     * GameManager gameManager = new GameManager(terminal, timer);
+     * gameManager.start();
+     */
+
     return 0;
 }
