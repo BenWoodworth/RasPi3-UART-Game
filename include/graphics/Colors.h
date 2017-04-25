0d1329beb4ff5a4706f18305d6038d3eacac26c6
Ben Woodworth <ben@benwoodworth.net>
2017-04-25 13:13:15 -0400

    Support terminals that only have limited colors.

--- include/graphics/Colors.h
+++ include/graphics/Colors.h
@@ -31,7 +31,7 @@ public:
 
     /// Convert the color into one of the 16 colors specified by
     /// the original standard. The result will be within the
-    /// ranges of 30-37 or 40-47.
+    /// ranges of 0-7 (normal), or 10-17 (bright).
     static uint8_t getBasicAnsiColor(uint32_t color) {
         // Get color levels on a scale of 0-3
         uint8_t red = getR(color) >> 6;
@@ -49,37 +49,37 @@ public:
         // Classify colors
         switch (rgb) {
             // Normal colors
-            case 0x100: return 31; // Red
+            case 0x100: return 1; // Red
             case 0x110:
-            case 0x120: return 33; // Yellow
-            case 0x010: return 32; // Green
+            case 0x120: return 3; // Yellow
+            case 0x010: return 2; // Green
             case 0x011:
-            case 0x012: return 36; // Cyan
+            case 0x012: return 6; // Cyan
             case 0x001:
-            case 0x101: return 34; // Blue
-            case 0x201: return 35; // Magenta
+            case 0x101: return 4; // Blue
+            case 0x201: return 5; // Magenta
             
             // Bright colors
-            case 0x200: return 41; // Red
+            case 0x200: return 11; // Red
             case 0x210:
-            case 0x220: return 43; // Yellow
-            case 0x020: return 42; // Green
+            case 0x220: return 13; // Yellow
+            case 0x020: return 12; // Green
             case 0x021:
-            case 0x022: return 46; // Cyan
-            case 0x002: return 44; // Blue
+            case 0x022: return 16; // Cyan
+            case 0x002: return 14; // Blue
             case 0x102:
-            case 0x202: return 45; // Magenta
+            case 0x202: return 15; // Magenta
 
             // Whites and Blacks
-            case 0x000: return 30; // Black
+            case 0x000: return 0; // Black
             case 0x111:
             case 0x112:
             case 0x121:
-            case 0x211: return 40; // Bright Black
+            case 0x211: return 10; // Bright Black
             case 0x122:
             case 0x212:
-            case 0x221: return 37; // Normal White
-            case 0x222: return 47; // White
+            case 0x221: return 7; // Normal White
+            case 0x222: return 17; // White
         }
     }
 };
