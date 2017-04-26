2e63b20f8a5aae5606ee4868e74e1dbae07acc25
Dan Fisher <dfishe33@gmail.com>
2017-04-26 18:45:40 -0400

    Wrote a NoOp loop to repace Thread sleeping

--- source/timer/TimerGcc.cpp
+++ source/timer/TimerGcc.cpp
@@ -1,6 +1,5 @@
 #include "timer/TimerGcc.h"
 #include <chrono>
-#include <thread>
 
 std::chrono::time_point<std::chrono::system_clock> startTime;
 
@@ -9,9 +8,13 @@ void TimerGcc::start() {
 }
 
 void TimerGcc::waitMicro(uint32_t us) {
-    auto now = std::chrono::system_clock::now().time_since_epoch();
     auto endTime = startTime.time_since_epoch() + std::chrono::microseconds(us);
-    if (now < endTime){
-        std::this_thread::sleep_for(endTime - now);
+    
+    auto now = std::chrono::system_clock::now().time_since_epoch();
+
+    
+    while(now < endTime){
+          now = std::chrono::system_clock::now().time_since_epoch();
     }
+
 }
