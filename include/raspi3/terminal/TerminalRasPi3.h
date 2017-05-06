4244a2583fc2a1d4d818ec0942b1a2307fb0e4cf
Ben Woodworth <ben@benwoodworth.net>
2017-05-06 02:04:03 -0400

    Implement RasPi3 Terminal. (Untested)

--- include/raspi3/terminal/TerminalRasPi3.h
+++ include/raspi3/terminal/TerminalRasPi3.h
@@ -5,6 +5,15 @@
 #include "terminal/Terminal.h"
 
 class TerminalRasPi3 : public Terminal {
+private:
+    uint64_t RPI_PERI_BASE_ADDR = 0x3F200000UL; // TODO Correct?
+
+    uint8_t UART0_DR = 0;
+    uint8_t UART0_FR = 6;
+    uint8_t RX_FIFO_EMPTY = 1 << 4;
+    uint8_t TX_FIFO_FULL = 1 << 5;
+
+    volatile uint32_t *uart = (uint32_t*)(RPI_PERI_BASE_ADDR + 0x1000);
 public:
     void printChar(uint8_t ch);
 
--- source/raspi3/terminal/TerminalRasPi3.cpp
+++ source/raspi3/terminal/TerminalRasPi3.cpp
@@ -1,24 +1,33 @@
+#include <string>
 #include <stdint.h>
 #include "terminal/TerminalRasPi3.h"
 
 void TerminalRasPi3::printChar(uint8_t ch) {
-    // TODO
+    // Wait until not full
+    while (((volatile uint32_t)uart[UART0_FR]) & TX_FIFO_FULL);
+
+    // Send character
+    uart[UART0_DR] = ch;
 }
 
 void TerminalRasPi3::printStr(std::string str) {
-    // TODO
+    for (int i = 0; str[i]; i++) {
+        printChar(str[i]);
+    }
 }
 
 void TerminalRasPi3::printNumDec(int32_t num) {
-    // TODO
+    std::to_string(num);
 }
 
 uint8_t TerminalRasPi3::getChar() {
-    // TODO
-    return 0;
+    // Wait for a character
+    while (!hasChar());
+
+    // Get the character
+    return (uint8_t)(uart[UART0_DR] & 0xFF);
 }
 
 bool TerminalRasPi3::hasChar() {
-    // TODO
-    return false;
+    return !(((volatile uint32_t) uart[UART0_FR]) & RX_FIFO_EMPTY);
 }
