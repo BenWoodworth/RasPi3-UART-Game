6690fed0814e184eac5bd2259b1d83dabd280318
Dan Fisher <dfishe33@gmail.com>
2017-05-07 17:17:58 -0400

    Added player header files

---
+++ include/core/games/tic-tac-toe/PlayerComputerRandom.h
@@ -0,0 +1,10 @@
+#ifndef PLAYERCOMPUTERRANDOM_H_
+#define PLAYERCOMPUTERRANDOM_H_
+
+#include "games/tic-tac-toe/Player.h"
+
+class PlayerComputerRandom : public Player{
+    // Nothing in here for now    
+}
+
+#endif
---
+++ source/core/games/tic-tac-toe/Board.cpp
@@ -0,0 +1,18 @@
+#include "games/tic-tac-toe/Board.h"
+
+Symbol Board::getWinningSymbol(){
+    // TODO    
+}
+
+std::vector<Point> Board::getPointsWithSymbol(Symbol sym){
+    // TODO
+}
+
+void Board::drawBoard(bool showSelected, AnsiImage* img){
+    // TODO
+}
+
+void Board::drawPoint(Point point, int32_t x, int32_t y, AnsiImage* img){
+    // TODO
+}
+
